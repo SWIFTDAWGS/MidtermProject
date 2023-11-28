08c54e7d596213c6d4826269500c4c93422f540b
stilexfps <cisasi100@mail.ru>
2023-11-28 22:00:29 +0400

    Second Commit

---
+++ test.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append("src")
+from src.predict import predict
+
+if __name__ == 'main':
+    # Example input data for prediction
+    test_data = [[5.1, 3.5, 1.4, 0.2], [6.2, 3.4, 5.4, 2.3]]
+
+    # Make predictions
+    predictions = predict(test_data)
+
+    # Print predictions
+    for i, pred in enumerate(predictions):
+        print(f'Prediction for example {i + 1}: {pred}')
